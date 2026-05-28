647305265a4921e98703e24f0c8067d75709d3b7
ivan scarciglia <ivan.scarciglia@gmail.com>
2026-05-28 22:21:25 +0200

    fix

--- src/hooks/permissions/usePermissions.ts
+++ src/hooks/permissions/usePermissions.ts
@@ -1,8 +1,7 @@
 import {useMemo} from 'react';
-import {useAppSelector} from '../store/store';
-import {ROLE_PERMISSIONS} from '../constants/permissions.constant';
-import type {Permission} from '../constants/permissions.constant';
-import type {UserRole} from '../features/auth/slice/auth.type';
+import {useAppSelector} from "../../store/store.ts";
+import {UserRole} from "../../features/auth/slice/auth.type.ts";
+import {Permission, ROLE_PERMISSIONS} from "../../constants/permissions.constant.ts";
 
 export const usePermissions = () => {
     const user = useAppSelector((state) => state.auth.user);
